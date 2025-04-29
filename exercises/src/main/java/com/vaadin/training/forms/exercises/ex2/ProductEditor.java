242f6a5674400c50811cbe8c6b0c67ede3409467
Jakub Gaba <j.gaba@email.cz>
2025-04-29 18:13:37 +0200

    FinishedFirstWay

--- exercises/src/main/java/com/vaadin/training/forms/exercises/ex2/ProductEditor.java
+++ exercises/src/main/java/com/vaadin/training/forms/exercises/ex2/ProductEditor.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
@@ -13,34 +14,32 @@ public class ProductEditor extends Composite<VerticalLayout> implements HasSize 
 
     private final TextField nameField = new TextField("Name");
     private final TextField priceField = new TextField("Price");
-    private final TextField ValidationField = new TextField("Validation");
+    private final DatePicker validationField = new DatePicker("Validation");
 
-    private final Binder<Product> binder = new Binder<>(Product.class);
+    private final Binder<Product> binder = new Binder<>(Product.class); //Bind with DAO 
 
     public ProductEditor(Product product, Consumer<Product> productConsumer) {
         VerticalLayout layout = getContent();
 
-        layout.add(nameField, priceField, ValidationField);
+        layout.add(nameField, priceField, validationField); //Adding into simple Layout
 
-        // Binds Product fields to UI fields
+
+        //Binding is happening !
         binder.bind(nameField, Product::getName, Product::setName);
         binder.forField(priceField)
-            .withConverter(Double::valueOf, String::valueOf, "Must be a number")
+            .withConverter(Double::valueOf, String::valueOf, "Must be a number") // Checking that it will gonna be number and then its converts it into String
             .bind(Product::getPrice, Product::setPrice);
-        
-
-        binder.readBean(product);
+        binder.bind(validationField, Product::getAvailable, Product::setAvailable);
 
         Button save = new Button("Save", event -> {
             if (binder.writeBeanIfValid(product)) {
-                productConsumer.accept(product); // Callback on success
+                productConsumer.accept(product);
             }
         });
 
         Button cancel = new Button("Cancel", event -> {
-            binder.readBean(product); // Reset form
+            binder.readBean(product); 
         });
-
         layout.add(save, cancel);
     }
 }
